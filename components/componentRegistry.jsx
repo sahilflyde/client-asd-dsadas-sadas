dcaae0dd9030eca5f23122796fbed3bf1dc73d01
Blinkflo Builder <builder@blinkflo.dev>
2026-01-06 08:51:20 +0000

    auto generated

---
+++ components/componentRegistry.jsx
@@ -0,0 +1,36 @@
+
+import HeroSection from "./heroSection";
+import TargetAudienceSectionComp from "./targetAudienceSection";
+import GridVideo from "./GridVideo";
+import WhyChoose from "./whyChoose";
+import FeaturesSection from "./featuresSection";
+import TransformSection from "./TransformSection";
+import MetricsSection from "./key-metric";
+import IntegrationSection from "./integrationsSection";
+import PricingSection from "./PricingSection";
+import FAQSection from "./FaqSection";
+import TestimonialSection from "./testimonial";
+import CareersHeroSection from "./careersHeroSection";
+import CareerWhyWork from "./careerWhyWork";
+import CareersOpenPosition from "./careersOpenPosition";
+import Header from "./header";
+import Footer from "./footer";
+
+export const componentRegistry = {
+  "hero-section": HeroSection,
+  "target-audience": TargetAudienceSectionComp,
+  "grid-video": GridVideo,
+  "why-choose": WhyChoose,
+  "feature-section": FeaturesSection,
+  "transform-section": TransformSection,
+  "metrics-section": MetricsSection,
+  "integration-section": IntegrationSection,
+  "pricing-section": PricingSection,
+  "faq-section": FAQSection,
+  "testimonials-section": TestimonialSection,
+  "careers-hero": CareersHeroSection,
+  "careers-why-work": CareerWhyWork,
+  "careers-open-positions": CareersOpenPosition,
+  "header-sec": Header,
+  "footer-section": Footer
+};
